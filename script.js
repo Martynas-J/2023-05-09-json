93787043bc516bc469696301a238ddc22404c0b0
Martynas-J <Aurimhas@gmail.com>
2023-05-17 10:44:19 +0300

    v1.0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+let moviesLists = document.querySelector(".movies-lists")
 
 getById()
 function getById() {
@@ -9,7 +9,7 @@ function getById() {
                 fetch(`movie-${item}.json`)
                     .then(response => response.json())
                     .then(data => { 
-                         console.log(data)
+                        output(data)
                     })
               
             })
@@ -17,60 +17,61 @@ function getById() {
         })
 }
 
-
-// get();
+get();
 function get() {
     fetch('movies.json')
         .then(response => response.json())
         .then(data => {
-            let moviesLists = document.querySelector(".movies-lists")
             data.map(item => {
-                let movie = document.createElement("div")
-                movie.classList.add("movie")
-                moviesLists.append(movie)
-                let h1 = document.createElement("h1")
-                let h2Ratings = document.createElement("h2")
-                let h2Genres = document.createElement("h2")
-                let h2Director = document.createElement("h2")
-                let h2Stars = document.createElement("h2")
-                let spanYear = document.createElement("span")
-                let pDescription = document.createElement("p")
-                let ulGenres = document.createElement("ul")
-                let ulDirector = document.createElement("ul")
-                let ulStars = document.createElement("ul")
-                let ulRatings = document.createElement("ul")
-
-                h1.textContent = item.name
-                spanYear.textContent = ` (${item.year}m.)`
-                pDescription.textContent = item.description
-                h2Ratings.textContent = "Ratings:"
-                h2Genres.textContent = "Genres:"
-                h2Director.textContent = "Director:"
-                h2Stars.textContent = "Stars:"
-                movie.append(h1, h2Ratings, ulRatings, pDescription, h2Genres, ulGenres, h2Director, ulDirector, h2Stars, ulStars)
-
-                let liEvaluation = document.createElement("li")
-                let liVotes = document.createElement("li")
-                liEvaluation.textContent = "Evaluation: " + item.ratings.evaluation
-                liVotes.textContent = "Votes: " + item.ratings.votes
-                ulRatings.append(liEvaluation, liVotes)
-
-                item.genres.map(item => {
-                    let li = document.createElement("li")
-                    li.textContent = item
-                    ulGenres.append(li)
-                })
-                item.director.map(item => {
-                    let li = document.createElement("li")
-                    li.textContent = item
-                    ulDirector.append(li)
-                })
-                item.stars.map(item => {
-                    let li = document.createElement("li")
-                    li.textContent = item
-                    ulStars.append(li)
-                })
-                h1.append(spanYear)
+                output(item)
             })
         })
+}
+function output(item) {
+    let movie = document.createElement("div")
+    movie.classList.add("movie")
+    moviesLists.append(movie)
+    let h1 = document.createElement("h1")
+    let h2Ratings = document.createElement("h2")
+    let h2Genres = document.createElement("h2")
+    let h2Director = document.createElement("h2")
+    let h2Stars = document.createElement("h2")
+    let spanYear = document.createElement("span")
+    let pDescription = document.createElement("p")
+    let ulGenres = document.createElement("ul")
+    let ulDirector = document.createElement("ul")
+    let ulStars = document.createElement("ul")
+    let ulRatings = document.createElement("ul")
+
+    h1.textContent = item.name
+    spanYear.textContent = ` (${item.year}m.)`
+    pDescription.textContent = item.description
+    h2Ratings.textContent = "Ratings:"
+    h2Genres.textContent = "Genres:"
+    h2Director.textContent = "Director:"
+    h2Stars.textContent = "Stars:"
+    movie.append(h1, h2Ratings, ulRatings, pDescription, h2Genres, ulGenres, h2Director, ulDirector, h2Stars, ulStars)
+
+    let liEvaluation = document.createElement("li")
+    let liVotes = document.createElement("li")
+    liEvaluation.textContent = "Evaluation: " + item.ratings.evaluation
+    liVotes.textContent = "Votes: " + item.ratings.votes
+    ulRatings.append(liEvaluation, liVotes)
+
+    item.genres.map(item => {
+        let li = document.createElement("li")
+        li.textContent = item
+        ulGenres.append(li)
+    })
+    item.director.map(item => {
+        let li = document.createElement("li")
+        li.textContent = item
+        ulDirector.append(li)
+    })
+    item.stars.map(item => {
+        let li = document.createElement("li")
+        li.textContent = item
+        ulStars.append(li)
+    })
+    h1.append(spanYear)
 }
