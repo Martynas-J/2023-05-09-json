b91517a31d84954ff99bb745484202dd5cabb88b
Martynas-J <Aurimhas@gmail.com>
2023-05-17 16:07:54 +0300

    v4.0

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let moviesLists = document.querySelector(".movies-lists")
 
-getById()
+// getById()
 function getById() {
     fetch('movies-by-id.json')
         .then(response => response.json())
@@ -8,16 +8,16 @@ function getById() {
             data.map(item => {
                 fetch(`movie-${item}.json`)
                     .then(response => response.json())
-                    .then(data => { 
+                    .then(data => {
                         output(data)
                     })
-              
+
             })
 
         })
 }
 
-get();
+// get();
 function get() {
     fetch('movies.json')
         .then(response => response.json())
@@ -75,3 +75,99 @@ function output(item) {
     })
     h1.append(spanYear)
 }
+let button = document.createElement("button")
+// jokeId.after(button)
+// button.textContent = "Joke"
+// button.addEventListener("click", getApi)
+// button.addEventListener("click", () => {
+//     getApi();
+// })
+// function getApi() {
+//     fetch('https://api.chucknorris.io/jokes/random')
+//         .then(response => response.json())
+//         .then(data => {
+//             jokeId.textContent = data.value
+//         })
+// }
+let submit = document.querySelector("#submit")
+let pJoke = document.querySelector("#joke")
+let form = document.querySelector("#joke-form")
+let categoryOptions = form.querySelector("#category")
+getCategories()
+function getCategories() {
+    fetch('https://api.chucknorris.io/jokes/categories')
+        .then(response => response.json())
+        .then(data => {
+            data.map(category => {
+                let categoryElement = document.createElement("option")
+                categoryElement.textContent = "- " + category.toUpperCase()
+                categoryElement.value = category
+                categoryOptions.append(categoryElement)
+            })
+            submit.removeAttribute("disabled")
+            submit.value = "Get joke"
+        })
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const form = event.target
+    let category = form.category.value
+    getJoke(category)
+})
+
+function getJoke(category) {
+    fetch(`https://api.chucknorris.io/jokes/random?category=${category}`)
+        .then(response => response.json())
+        .then(data => {
+            pJoke.textContent = data.value
+        })
+}
+
+
+
+
+let textForm = document.querySelector("#joke-by-text-form")
+let textSubmit = document.querySelector("#submit-text")
+let categoryByTextOptions = textForm.querySelector("#category-by-text")
+
+getCategories()
+function getCategories() {
+    fetch('https://api.chucknorris.io/jokes/categories')
+        .then(response => response.json())
+        .then(data => {
+            data.map(category => {
+                let categoryElement = document.createElement("option")
+                categoryElement.textContent = "- " + category.toUpperCase()
+                categoryElement.value = category
+                categoryByTextOptions.append(categoryElement)
+            })
+            textSubmit.removeAttribute("disabled")
+            textSubmit.value = "Get joke"
+        })
+}
+
+textForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const form = event.target
+    let text = form.text.value
+    let category = form['category-by-text'].value
+    getJokeByText(text, category)
+})
+
+function getJokeByText(text, category) {
+    fetch(`https://api.chucknorris.io/jokes/search?query=${text}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.result)
+
+            let categoryFilter = data.result.filter(item => item.categories.some(categoryItem => categoryItem === category));
+            let randomJokeNr = Math.floor(Math.random() * categoryFilter.length)
+            pJoke.textContent = categoryFilter[randomJokeNr].value
+        })
+        .catch(error => {
+            pJoke.textContent = "not found"
+        });
+}
+
+
