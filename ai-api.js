4398f6fe5cdce98597881f13c19bb090c689423a
Martynas-J <Aurimhas@gmail.com>
2023-05-19 10:15:01 +0300

    v1.0 su async

--- ai-api.js
+++ ai-api.js
@@ -31,72 +31,68 @@ let personGenderProbability
 let personGenderCount
 let personAge
 
-personDataForm.addEventListener("submit", (event) => {
+personDataForm.addEventListener("submit", async (event) => {
     event.preventDefault()
     let form = event.target
     let name = form.name.value
     personName = name
-    nationalApi(name)
-    genderApi(name)
-    ageApi(name)
+    ulNational.innerHTML = "";
+    ulGender.innerHTML = "";
+    ulAge.innerHTML = "";
+    outputText.innerHTML = ""
+    await nationalApi(name)
+    await genderApi(name)
+    await ageApi(name)
+    outputPersonNameInfo()
     titleCounty.textContent = `Data by name: ${name}`
+
 })
 
-function nationalApi(name) {
-    ulNational.innerHTML = "";
-    fetch(`https://api.nationalize.io/?name=${name}`)
-        .then(response => response.json())
-        .then(data => {
-            personNameProbability = data.country[0].probability
-            personCountry_id = data.country[0].country_id
-            data.country.forEach(element => {
-                let liCountyId = document.createElement("li")
-                let liCountyProbability = document.createElement("li")
-                titleNational.textContent = "By National:"
-                liCountyId.innerHTML = `<b>Country:</b> ${element.country_id}`
-                liCountyProbability.innerHTML = `<b>Probability: </b>${element.probability} %`
-                ulNational.append(liCountyId, liCountyProbability)
-            });
-            outputPersonNameInfo()
-        })
-        
+async function nationalApi(name) { 
+    const response = await fetch(`https://api.nationalize.io/?name=${name}`)
+    const data = await response.json()
+    personNameProbability = data.country[0].probability
+    personCountry_id = data.country[0].country_id
+    data.country.forEach(element => {
+        let liCountyId = document.createElement("li")
+        let liCountyProbability = document.createElement("li")
+        titleNational.textContent = "By National:"
+        liCountyId.innerHTML = `<b>Country:</b> ${element.country_id}`
+        liCountyProbability.innerHTML = `<b>Probability: </b>${element.probability} %`
+        ulNational.append(liCountyId, liCountyProbability)
+    })
 }
 
-function genderApi(name) {
-    ulGender.innerHTML = "";
-    fetch(`https://api.genderize.io?name=${name}`)
-        .then(response => response.json())
-        .then(data => {
-            personGender = data.gender
-            personGenderProbability = data.probability
-            personGenderCount = data.count
-            let liCount = document.createElement("li")
-            let liGender = document.createElement("li")
-            let liProbability = document.createElement("li")
-            titleGender.textContent = "By Gender:"
-            liCount.innerHTML = `<b>Count:</b> ${data.count}`
-            liGender.innerHTML = `<b>Gender: </b>${data.gender}`
-            liProbability.innerHTML = `<b>Probability: </b>${data.probability} %`
-            ulGender.append(liGender, liCount, liProbability)
-        })
+async function genderApi(name) {
+    const response = await fetch(`https://api.genderize.io?name=${name}`)
+    const data = await response.json()
+    personGender = data.gender
+    personGenderProbability = data.probability
+    personGenderCount = data.count
+    let liCount = document.createElement("li")
+    let liGender = document.createElement("li")
+    let liProbability = document.createElement("li")
+    titleGender.textContent = "By Gender:"
+    liCount.innerHTML = `<b>Count:</b> ${data.count}`
+    liGender.innerHTML = `<b>Gender: </b>${data.gender}`
+    liProbability.innerHTML = `<b>Probability: </b>${data.probability} %`
+    ulGender.append(liGender, liCount, liProbability)
+
 }
-function ageApi(name) {
-    ulAge.innerHTML = "";
-    fetch(`https://api.agify.io/?name=${name}`)
-        .then(response => response.json())
-        .then(data => {
-            personAge = data.age
-            let liCount = document.createElement("li")
-            let liAge = document.createElement("li")
-            titleAge.textContent = "By Age:"
-            liCount.innerHTML = `<b>Count:</b> ${data.count}`
-            liAge.innerHTML = `<b>Age: </b>${data.age}`
-            ulAge.append(liAge, liCount)
-        })
+async function ageApi(name) {
+    const response = await fetch(`https://api.agify.io/?name=${name}`)
+    const data = await response.json()
+    personAge = data.age
+    let liCount = document.createElement("li")
+    let liAge = document.createElement("li")
+    titleAge.textContent = "By Age:"
+    liCount.innerHTML = `<b>Count:</b> ${data.count}`
+    liAge.innerHTML = `<b>Age: </b>${data.age}`
+    ulAge.append(liAge, liCount)
+
 }
 let outputText = document.createElement("p")
 function outputPersonNameInfo() {
-    outputText.innerHTML = `Name ${personName} by nationality (highest percentage ${personNameProbability} %) is from  country ${personCountry_id}. ${personName} according to gender is ${personGender} (${personGenderProbability}%). We count ${personGenderCount} ${personGender} ${personName}. ${personName} average by age is ${personAge} year` 
-
+    outputText.innerHTML = `Name ${personName} by nationality (highest percentage ${personNameProbability} %) is from  country ${personCountry_id} . ${personName} according to gender is ${personGender} (${personGenderProbability}%). We count ${personGenderCount} ${personGender} ${personName}. ${personName} average by age is ${personAge} year`
     personDataForm.before(outputText)
 }
